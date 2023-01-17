120573006b48cdf8cec651abd5a1efdcf03de006
Matt Bommer <matthew.bommer@willowtreeapps.com>
2023-01-17 13:59:00 -0500

    Remove class validation from feed dto

--- src/feed/feed.entity.ts
+++ src/feed/feed.entity.ts
@@ -1,23 +1,16 @@
 import { Column, ManyToOne, PrimaryGeneratedColumn, Entity } from 'typeorm';
-import { IsNotEmpty, IsDefined } from 'class-validator';
 import { User } from 'src/user/user.entity';
 
 @Entity()
 export class RssFeed {
     
     @PrimaryGeneratedColumn('uuid')
-    @IsNotEmpty()
-    @IsDefined()
     resourceId: string
     
     @Column()
-    @IsNotEmpty()
-    @IsDefined()
     url: string
     
     @Column()
-    @IsNotEmpty()
-    @IsDefined()
     title: string
 
     @Column({ nullable: true })
